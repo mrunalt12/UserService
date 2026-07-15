c63fd65f3a1ba8a11c085efef5609467774762ad
mtarnekar <mtarnekar408@gmail.com>
2026-07-15 21:17:20 +0530

    Added Service

---
+++ UserServiceInventary/src/main/java/com/inventary/india/UserServiceInventaryApplication.java
@@ -0,0 +1,13 @@
+package com.inventary.india;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceInventaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceInventaryApplication.class, args);
+	}
+
+}
---
+++ UserServiceInventary/src/test/java/com/inventary/india/UserServiceInventaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventary.india;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceInventaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
